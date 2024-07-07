a5f6dc6250abfd23a861ec37a819bc1bfea2bb7b
reginaldo <fumorefina92@gmail.com>
2024-07-07 08:08:08 +0200

    Descrição das mudanças feitas

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../')));
 
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    console.log(`Email: ${email}, Password: ${password}`);
-    res.send('Login data received');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.post('/submit', (req, res) => {
+    const { email, password } = req.body;
+    console.log(`Email: ${email}, Password: ${password}`);
+    res.send('Form data received');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
